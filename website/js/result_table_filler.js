81b6f07b95f4af1dee8c7dd046ca79301481fea5
Jonas Konrad <jonas.konrad@epfl.ch>
2022-05-26 17:27:03 +0200

    add time formatting

--- website/js/result_table_filler.js
+++ website/js/result_table_filler.js
@@ -4,6 +4,18 @@
 
 */
 
+function format_seconds(seconds) {
+    d = Number(seconds);
+    var hv = Math.floor(d / 3600);
+    var mv = Math.floor(d % 3600 / 60);
+    var sv = Math.floor(d % 3600 % 60);
+
+    var h = hv > 0 ? hv + (hv == 1 ? " hour, " : " hours, ") : "";
+    var m = mv > 0 ? mv + (mv == 1 ? " minute, " : " minutes, ") : "";
+    var s = sv > 0 ? sv + (sv == 1 ? " second" : " seconds") : "";
+    
+    return h + m + s; 
+}
 
 function fill_stage_result_table(year, stage_number) {
     selected_stage_data = stage_data.filter(function(data) {
@@ -13,6 +25,6 @@ function fill_stage_result_table(year, stage_number) {
 
     table.clear()
     selected_stage_data.forEach(stage_data => {
-        table.row.add([stage_data.rank, stage_data.rider, stage_data.team, stage_data.time_gap_to_winner_sec]).draw(false);
+        table.row.add([stage_data.rank, stage_data.rider, stage_data.team, stage_data.time_sec, stage_data.time_gap_to_winner_sec]).draw(false);
     });
 }
