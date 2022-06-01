46cf19b40fd7d18d7788a1da7b48d58ce6428b31
nana0211 <49521008+nana0211@users.noreply.github.com>
2022-06-01 10:33:53 +0200

    add stage and edition stats (#71)
    * update
    
    * added team and starters

--- website/js/result_table_filler.js
+++ website/js/result_table_filler.js
@@ -30,6 +30,36 @@ function fill_stage_result_table(year, stage_number) {
     });
 }
 
+function fill_stage_result_information(year,stage) {
+
+    var starting_date = document.getElementById("stage_date");
+    var distance = document.getElementById("stage_distance");
+    var type = document.getElementById("stage_type");
+    var origin = document.getElementById("start_point");
+    var finish = document.getElementById("end_point");
+    var winner_ = document.getElementById("winner");
+    var winner_country_ = document.getElementById("winner_country");
+    var flag = document.getElementById("winner_flag");
+    selected_stage_information = stages.filter(function (data) {
+        return (data.year == year) && (data.stage == stage)
+    })
+    starting_date.innerHTML = ("Start Date: " +  selected_stage_information[0].date);
+    distance.innerHTML = ("Distance: " +  selected_stage_information[0].distance_km + ' km');
+    type.innerHTML  = ("Stage Type: " +  selected_stage_information[0].type);
+    origin.innerHTML  =  ("Starting point: " + selected_stage_information[0].origin);
+    finish.innerHTML  = ("End point: " + selected_stage_information[0].destination);
+    winner_.innerHTML = ("Winner: " + selected_stage_information[0].winner);
+    
+    winner_country = winners.filter(function (data) {
+        return (data.winner_name == selected_stage_information[0].winner)
+    })
+    /*winner_flag = flags.filter(function (data) {
+        return (data.country == winner_country[0].nationality)
+    })*/
+    /*issue1 this winner country does not update accordingly*/
+    winner_country_.innerHTML= ("WinnerCountry: " + winner_country[0].nationality);
+    /*flag.innerHTML = ("Country_flag: " + winner_flag[0].flag_symbol);*/
+}
 
 function fill_edition_result_table(year) {
     selected_edition = edition_data.filter(function(edition) {
@@ -72,3 +102,46 @@ function fill_jersey_winner(year) {
         table.select("#winner_name_" + key).text(value)
     })
 }
+
+function fill_edition_result_information(year){
+
+    var edition_date = document.getElementById("edition_date");
+    var edition_end = document.getElementById("edition_end");
+    var num_of_stages = document.getElementById("num_of_stages");
+    var edition_distance = document.getElementById("edition_distance");
+    var total_teams = document.getElementById("total_teams");
+    var total_riders = document.getElementById("total_riders");
+    var number_of_stages = new Set()
+    var dates = new Set()
+    var distances = new Set()
+    var teams = new Set()
+    var starters = new Set()
+    edition_stats = stages.filter(function (data) {
+        return (data.year == year)
+    })
+    edition_stats.forEach(edition_stat => {
+        number_of_stages.add(edition_stat.stage);
+        dates.add(edition_stat.date);
+        distances.add(parseInt(edition_stat.distance_km));
+        teams.add(edition_stat.team)
+        starters.add(edition_stat.rider)
+    })
+   
+    edition_date.innerHTML = ("Edition date: " +  dates.values().next().value);
+    edition_end.innerHTML = ("End of edition: " +  Array.from(dates).pop());
+    num_of_stages.innerHTML = ("Numbers of stages: " +  Array.from(number_of_stages).pop());
+    sum_distance =  Array.from(distances).reduce((a, b) => a + b, 0)
+    edition_distance.innerHTML  = ("Total Distance: " +  sum_distance +' km');
+
+    selected_stage_data = stage_data.filter(function (data) {
+        return (data.year == year) 
+    })
+    selected_stage_data.forEach(select_date => {
+    teams.add(select_date.team)
+    starters.add(select_date.rider)
+    })  
+    total_riders.innerHTML = ("Total Starters: " +  starters.size);
+    total_teams.innerHTML = ("Total teams: " +  teams.size);
+
+}
+
--- website/js/sidebar_utils.js
+++ website/js/sidebar_utils.js
@@ -4,8 +4,9 @@
 
 */
 
-
+var flags;
 var stages;
+var winners;
 var locations;
 var stage_data;
 var jerseys_data;
@@ -20,13 +21,16 @@ function init_edition_selection(callback) {
         d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/stage_data.csv"),
         d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/jerseys.csv"),
         d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/TDF_Riders_History.csv"),
+        d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/tdf_winners.csv"),
+        d3.csv("https://raw.githubusercontent.com/com-480-data-visualization/datavis-project-2022-datawiz/master/data/flags.csv"),
     ]).then(function(initialize) {
         locations = initialize[0];
         stages = initialize[1]
         stage_data = initialize[2]
         jerseys_data = initialize[3]
         edition_data = initialize[4]
-
+        winners = initialize[5]
+        flags = initialize[6]
         const years = new Set()
         stages.forEach(stage => {
             years.add(stage.year);
@@ -35,6 +39,7 @@ function init_edition_selection(callback) {
         var starting_year = "2017"
 
         fill_edition_select(years, starting_year)
+        fill_edition_result_information(starting_year)
         changeEdition(starting_year)
         callback()
     });
@@ -58,15 +63,17 @@ function changeEdition(edition_year) {
     })
 
     fill_stage_select(edition_year, stage_numbers)
+    fill_stage_result_information(edition_year, 1)
     fill_jersey_winner(edition_year)
     fill_edition_result_table(edition_year);
-
+    
     // Update stage change
     $('#stage_select').on('change', function() {
         // Update which results are displayed
         var selected_stage = $(this).val();
         fill_stage_result_table(edition_year, selected_stage)
-
+        fill_edition_result_information(edition_year)
+        fill_stage_result_information(edition_year, selected_stage)
         // Update which path is higlighted
         reset_all_paths_states()
         var link = d3.selectAll(".leaflet-interactive.stage_link")
